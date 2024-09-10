4b6a928976628012b4047d2dbd3acf64f1dae144
zaim <zaimsagheer@gmail.com>
2024-09-11 02:05:17 +0500

    implement edit and add button updating both

--- gui.py
+++ gui.py
@@ -3,21 +3,38 @@ import functions
 
 label = sg.Text("Enter a todo: ")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-button = sg.Button("Add")
-
-window = sg.Window("Todo App", layout=[[label],
-                                            [input_box], [button]],
+add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
+window = sg.Window("Todo App", layout=[[label],[input_box,add_button], [list_box, edit_button]],
                                     font=("Helvatica",20))
 while True:
-    event, value = window.read()
-    print(event)
-    print(value)
+    event, values = window.read()
+
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = value['todo'] + "\n"
+            new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + '\n'
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+
         case sg.WINDOW_CLOSED:
             break
 window.close()
