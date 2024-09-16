cd8eb01c9722c937d72bc2204bb3d902c43f4219
zaim <zaimsagheer@gmail.com>
2024-09-16 21:41:42 +0500

    implementing add function.

--- webapp.py
+++ webapp.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import functions
 
-todos= functions.get_todos()
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+    print(todo)
+
 st.title("My Todo App")
 st.subheader("This app is to improve your productivity.")
 st.write("This is my todo app")
@@ -9,4 +16,5 @@ st.write("This is my todo app")
 for todo in todos:
     st.checkbox(todo.title())
 
-st.text_input(label="Enter todo", placeholder="Add new todo...")
+st.text_input(label="Enter todo", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
