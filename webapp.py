ef737c0af08c4f0bbb469df42f1945bf42455533
zaim <zaimsagheer@gmail.com>
2024-09-17 00:58:53 +0500

    implement add and complete function.

--- webapp.py
+++ webapp.py
@@ -7,14 +7,20 @@ def add_todo():
     todo = st.session_state["new_todo"] + '\n'
     todos.append(todo)
     functions.write_todos(todos)
-    print(todo)
 
 st.title("My Todo App")
 st.subheader("This app is to improve your productivity.")
 st.write("This is my todo app")
 
-for todo in todos:
-    st.checkbox(todo.title())
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo , key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Enter todo", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
+
+st.session_state
