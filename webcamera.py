0da438ffa418a27d1cb03c884ba471f1f9ba021c
zaimsalimi123 <130406225+zaimsalimi123@users.noreply.github.com>
2025-02-27 21:50:32 +0500

    Both uploading and real time camera image conversion

--- webcamera.py
+++ webcamera.py
@@ -4,14 +4,22 @@ from PIL import Image
 uploaded_image = st.file_uploader("Upload Image")
 
 with st.expander("Start Camera"):
-#start the camera
+    #start the camera
     camera_image = st.camera_input("Camera")
 
 if camera_image:
-    #create a pillow image instance
-    img = Image.open(uploaded_image)
-
-    #Convert the pillow image to grayscale
+    # create a pillow image instance
+    img=Image.open(camera_image)
+    # convert image to grayscale
     gray_img = img.convert("L")
-    #Render the grayscale image on the webpage
+    # render the image on website
     st.image(gray_img)
+
+if uploaded_image:
+    
+    img_up = Image.open(uploaded_image)
+
+    gray = img_up.convert("L")
+
+    st.image(gray)
+
